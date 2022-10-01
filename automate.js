16019a9646b3235547c425d04e5d3df224db3b46
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2022-10-01 02:41:11 +0100

    Update automate.js

--- automate.js
+++ automate.js
@@ -17,6 +17,11 @@ axios.get('https://mudremcloudstore.aremzy.repl.co/')
 .then(function (response){  console.log(response); }) 
 .catch(function (error) {  console.log(error)})
 }
+
+axios.get('http://cryptocoinsmart.crypsc.repl.co/') 
+.then(function (response){  console.log(response); }) 
+.catch(function (error) {  console.log(error)})
+}
 console.log("started")
 setInterval(automate , 600000)
 require("http").createServer().listen(process.env.PORT)
